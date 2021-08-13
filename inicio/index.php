65109f77ae044d26fa4f61e23622b09bcb916d33
laincognita <laincognita3roemt@gmail.com>
2021-08-12 21:11:42 -0300

    Nuevo codigo

---
+++ inicio/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Inicio</title>
+</head>
+<body>
+<!-- DIVS -->
+<!-- DIV ENCABEZADO-->
+<div class="fila" id="encabezado">
+    <h1 class="titulo">MELOTRABAJA</h1>
+
+    </div>
+<!-- DIV BARRA-->
+<div class="fila" id="barra">
+    <div id="iconoMenu"></div>
+    <a href="login.php">Acceder</a>
+    <div id="iconoLupa"></div>
+
+</div>
+
+<!-- DIV CONTENIDO -->
+<div class="fila" id="contenido">
+    <div id="bordeContenido"></div>
+
+<hr></hr>
+    </div>
+<!-- DIV PIE -->
+<div class="fila" id="pie">
+
+    </div>
+<!-- /DIVS -->
+
+
+
+</body>
+</html>
