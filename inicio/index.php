a7f7c0450e0c86f105e33a87d06fd8e4ca5eff3e
laincognita <laincognita3roemt@gmail.com>
2021-08-24 20:50:27 -0300

    Introducciòn codigo

--- inicio/index.php
+++ inicio/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,22 +10,67 @@
 <!-- DIVS -->
 <!-- DIV ENCABEZADO-->
 <div class="fila" id="encabezado">
-    <h1 class="titulo">MELOTRABAJA</h1>
-
+    <div id="meloTrabaja"></div>
     </div>
 <!-- DIV BARRA-->
 <div class="fila" id="barra">
     <div id="iconoMenu"></div>
-    <a href="login.php">Acceder</a>
+    <a href="../login/index.php">Acceder</a>
     <div id="iconoLupa"></div>
 
 </div>
 
 <!-- DIV CONTENIDO -->
 <div class="fila" id="contenido">
-    <div id="bordeContenido"></div>
+    <div id="bordeContenido">
+        <div id="contenidoTop" class="separacionContenido">
+        <p>Publicaciones Fijadas</p>
+            <div id="fijado" class="publicaciones">
+                <p id="titulo">Reprehenderit ut quos aute excepteur temporibus </p>
+                <p id="nombreuser">Adm1</p>
+                <p id="fecha">10/08/2021</p>
+            </div>
+            <div id="fijado" class="publicaciones">
+            <p id="titulo">Voluptatem nostrum sequi delectus sint autem ex</p>
+                <p id="nombreuser">Adm2</p>
+                <p id="fecha">10/04/2021</p>
+            </div>
+            <div id="fijado" class="publicaciones">
+            <p id="titulo">Totam dignissimos reprehenderit et nisi </p>
+                <p id="nombreuser">Adm3</p>
+                <p id="fecha">15/07/2021</p>
+            </div>
+            <div id="fijado" class="publicaciones">
+            <p id="titulo">Aut saepe do in accusamus dolore aliquip et d</p>
+                <p id="nombreuser">Adm4</p>
+                <p id="fecha">05/02/2021</p>
+            </div>
+</div>
+        <div class="separacionContenido">
+        <p>Publicaciones Recientes</p>
+            <div id="recientes" class="publicaciones">
+            <p id="titulo">Et quia omnis nemo molestias cu</p>
+                <p id="nombreuser">User1</p>
+                <p id="fecha">30/12/2021</p>
+            </div>
+            <div id="recientes" class="publicaciones">
+            <p id="titulo">Voluptas nobis modi nesciunt error </p>
+                <p id="nombreuser">User2</p>
+                <p id="fecha">02/02/2021</p>
+            </div>
+            <div id="recientes" class="publicaciones">
+            <p id="titulo">Est placeat cumque dolor est enim</p>
+                <p id="nombreuser">User3</p>
+                <p id="fecha">05/08/2021</p>
+            </div>
+            <div id="recientes" class="publicaciones">
+            <p id="titulo">Suscipit omnis irure enim ut irure omnis</p>
+                <p id="nombreuser">User4</p>
+                <p id="fecha">01/06/2021</p>
+            </div>
+        </div>
+    </div>
 
-<hr></hr>
     </div>
 <!-- DIV PIE -->
 <div class="fila" id="pie">
