a58b298e3f6cb4fd76f8abb2c762fc8d8850dd09
Incognita19 <82481297+Incognita19@users.noreply.github.com>
2021-09-14 16:02:49 -0300

    Update BdD_Proyecto.sql

--- bdd/BdD_Proyecto.sql
+++ bdd/BdD_Proyecto.sql
@@ -17,9 +17,9 @@ ID_perfil  varchar (16) not null unique primary key,
 --Aca estan los atributos que debe tener el Usuario para registrarse--
 create table REGISTRO_USUARIO(
 nombre_ususario varchar (12) not null unique primary key,
-nombre varchar(12) not null no unique,
-apellido varchar(15) not null no unique,
-correo_electronico varchar(40) not null no unique,
+nombre varchar(12) not null,
+apellido varchar(15) not null,
+correo_electronico varchar(40) not null,
 );
 --LOS ATRIBUTOS QUE TIENE EL USUARIO EN EL INICIO  DE LA APLICACIÓN--
 create table INICIO_USUARIO(
@@ -30,24 +30,24 @@ create table INICIO_USUARIO(
 --LA ENTIDAD DE LA SESION EN LA APLICACIÓN-- 
 create table sesion(
 ID_sesion varchar(90) UNIQUE NOT NULL primary KEY,
-estado enum ("active","closed") NO NULL DEFAULT "active",
+estado enum ("active","closed") NOT NULL DEFAULT "active",
 tiempo_inicio timestamp NOT NULL DEFAULT CORRECT_TIMESTAMP,
 tiempo_fin timestamp 
 );
 --CONTIENE LA INFORMACION DE LOS ATRIBUTOS DEL ADMINISTRADOR--
 create table ADMINISTRADOR(
-nombre varchar(12) not null not unique,
-nombre varchar(12) not null  no unique,
-apellido varchar(15) not null no unique,
-correo_electronico varchar(40) not null no unique,
+nombre varchar(12) not null,
+nombre varchar(12) not null,
+apellido varchar(15) not null,
+correo_electronico varchar(40) not null,
 CI int(8)  not null  unique primary key,
 );
 --REGISTRA LAS LA INFORMACIÓN DE LAS PUBLICACIÓNES-- 
 create table POST(
 nombre_ususario varchar (12) not null unique,
-fecha_y_hora date(14) not null no unique,
-contenido varchar(120) not null no unique,
-titulo varchar(12) null not unique,
+fecha_y_hora date(14) not null,
+contenido varchar(120) not null,
+titulo varchar(12) not null,
 ID varchar(16) not null unique primary key,
 nombre_categoria varchar(20) not null unique, 
 );
@@ -68,28 +68,28 @@ descripcion (500)varchar not null unique,
 --Muestra la relación entre administrador perfil--
 
   create table HABILITA(
-     administrador_nombre varchar(12) not null no unique,
-     ID_perfil varchar(16) not null no unique FK,
+     administrador_nombre varchar(12) not null,
+     ID_perfil varchar(16) not null,
  );
 --Muestra la relación entre administrador y usuario--
    create table APRUEBA(
-     administrador_nombre varchar(12) not null no unique,
-     CI:int(8) not null no unique primary key FK,
+     administrador_nombre varchar(12) not null,
+     CI:int(8) not null primary key,
  );
 --Muestra la relación entre usuario y post--
  create table PUBLICA(
-     nombre_ususario varchar (12) no null no unique,
-     Id_post varchar (16) no null unique FK,
+     nombre_ususario varchar (12) not null,
+     Id_post varchar (16) not null unique,
  );
 --Muestra la relación entre post y categoria--
   create table TIENE(
-      nombre_categorias varchar(20) no null unique,
-      Id_categorias varchar (16) no null unique,
+      nombre_categorias varchar(20) not null unique,
+      Id_categorias varchar (16) not null unique,
  );
 --Muestra la relación entre adminstrador y categorias--
   create table DEFINE( 
-     nombre_administrador varchar(20) no null unique,
-     Id_categorias varchar (16) no null unique,
+     nombre_administrador varchar(20) not null unique,
+     Id_categorias varchar (16) not null unique,
  );
 --Muestra la relación entre usuario y la sesion--
  create table inicia(
