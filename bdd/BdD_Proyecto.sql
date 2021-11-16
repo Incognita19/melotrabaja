95168e75240be54ea32b39c2e675fa8723b40d98
incognita19 <Incognita1234>
2021-11-16 20:53:32 -0300

    nuevo codigo

--- bdd/BdD_Proyecto.sql
+++ bdd/BdD_Proyecto.sql
@@ -24,40 +24,40 @@ administrador date not null,
 email varchar (24) not null);
 
 create table trabajo (
-    curriculum varchar (500) not null,
-    descripción_profesional varchar (4000) not null,
+    curriculum text not null,
+    descripción_profesional text not null,
     id int(8) not null unique auto_increment primary key);
 
 create table categoria(
-descripcion varchar (4000) not null unique,
+descripcion text not null ,
 id int (8) not null auto_increment unique primary key);
 
 create table publicacion(
 hora date not null,
 titulo varchar (24) not null,
 contenido varchar (400) not null,
-id int(8) not null unique auto_increment primary key);
+id_publicacion int(8) not null unique auto_increment primary key);
 
 create table sesion(
 nombre varchar(12) not null,
-fecha-inicio date not null,
-fecha-final date not null,
+fecha_inicio date not null,
+fecha_final date not null,
 id int (8) not null unique auto_increment primary key);
 
 create table tiene (
-    nombreuser varchar (12) not null unique 
+    nombreuser varchar (12) not null unique,
     id_tu_perfil int (8) not null unique  
 );
 
 create table contiene(
-    id_tu_perfil int (8) not null unique, 
+    id_tu_perfil varchar (12) not null unique, 
     id_trabajo int (8) not null unique
 
 
 );
 
 create table especializa(
-    id_trabajo int (8) not null unique ,
+    id_trabajo int (8) not null unique,
     id_categoria int(8)  not null unique 
 );
 create table posee(
@@ -77,55 +77,55 @@ create table inicia(
 );
 
 alter table tiene
-add constraint FK_nombre_user,
-foreign key (nombreuser),
-references usuario(nombreuser),
-on update cascade,
+add constraint FK_nombre_user
+foreign key (nombreuser)
+references usuario(nombreuser)
+on update cascade
 on delete cascade;
 
 alter table tiene
- add constraint FK_id_tu_perfil,
- foreign key (id_tu_perfil),
- references tu_perfil(id),
-on update cascade,
+ add constraint FK_id_tu_perfil
+ foreign key (id_tu_perfil)
+ references tu_perfil(id)
+on update cascade
 on delete cascade;
 
 alter table hace
-    add constraint FK_nombreuser, 
-    foreign key (nombreuser),
-    references usuario(nombreuser),
-    on update cascade,
+    add constraint FK_nombreuser
+    foreign key (nombreuser)
+    references usuario(nombreuser)
+    on update cascade
     on delete cascade;
     
 alter table hace
-    add constraint FK_id_publicaciones,
-    foreign key (id_publicaciones),
-    references publicaciones(id),
-    on update cascade,
+    add constraint FK_id_publicaciones
+    foreign key (id_publicaciones)
+    references publicaciones(id)
+    on update cascade
     on delete cascade; 
 
 alter table inicia
-    add constraint FK_nombreusuario,
-    foreign key (nombreuser),
-    references usuario(),
-    on update cascade,
+    add constraint FK_nombreusuario
+    foreign key (nombreuser)
+    references usuario(nombreuser)
+    on update cascade
     on delete cascade;
 
 alter table inicia
-    add constraint FK_id_sesion,
-    foreign key (id_sesion),
-    references sesion (id),
-    on update cascade,
+    add constraint FK_id_sesion
+    foreign key (id_sesion)
+    references sesion (id)
+    on update cascade
     on delete cascade;
 
 alter table posee 
-    add constraint FK_publicaciones,
+    add constraint FK_publicaciones
     foreign key (id_publicaciones)
     references publicaciones(id)
     on update cascade
     on delete cascade;
 
-alter table poseee
+alter table posee
     add constraint FK_categoria
     foreign key (id_categoria)
     references categoria(id)
